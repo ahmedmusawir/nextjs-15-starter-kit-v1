159e018cc802e13be16727bc1080ae84897704d2
Ahmed Rahat Musawir <ahmed.musawir@gmail.com>
2025-06-01 14:13:16 +0800

    1june2025 - just upgraded next.js 15.2.3 and patched the security update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [ 
+      {
+        protocol: 'https', // Specify the protocol (e.g., 'https')
+        hostname: 'res.cloudinary.com', // Your image domain
+        // You can add pathname if you want to restrict to specific paths on Cloudinary
+        // pathname: '/image/upload/**', // Example: only allow images from /image/upload/
+      },
+    ],
   },
 };
 
